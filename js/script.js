8c97b3ba98a0fa3fcc9672e878317319d1636cff
Methuselah <dstengac@gmail.com>
2024-02-11 21:59:31 +0100

    commit 1

--- js/script.js
+++ js/script.js
@@ -1,18 +1,7 @@
 'use strict';
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const obj = {
+  mainw: 131212,
+}
 
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
-};
-
-const lastFilm = prompt('Один из последних просмотренных фильмов', ''),
-  lastFilmGrade = prompt("На сколько оцените его?'", '');
-
-personalMovieDB.movies[lastFilm] = lastFilmGrade;
-
-console.log(personalMovieDB);
+console.log(obj);
