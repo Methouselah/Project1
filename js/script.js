5480602149459f2f3903407e10c0dd2f922a06c8
Methuselah <dstengac@gmail.com>
2024-02-15 22:12:09 +0100

    commi **

--- js/script.js
+++ js/script.js
@@ -2,59 +2,43 @@
 
 // Место для первой задачи
 function firstTask() {
-    for (let i = 5; i <= 10; i++) {
-		console.log(i);
+    const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
+
+	for(let i = 0; i < arr.length; i++) {
+		result[i] = arr[i];
 	}
+
+	console.log(result);
+    return result;
 }
 // Место для второй задачи
 function secondTask() {
-    // Пишем решение вот тут
-    for (let i = 20; i >= 10; i--) {
-		if (i === 13) {
-            break;
-        }
-        console.log(i);
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    
+    for (let i = 0; i < data.length; i++) {
+		if (typeof data[i] === 'number') {
+			data[i] *= 2;
+		} else if (typeof data[i] === 'string')  {
+			data[i] += ' - done';
+		}
 	}
+
+	console.log(data);
+    return data;
 }
+
+
 
 // Место для третьей задачи
 function thirdTask() {
-    // Пишем решение вот тут
-    for (let i = 2; i <= 10; i++) {
-		if (i % 2 === 0) {
-            console.log(i);
-        }
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    const result = [];
+
+	for (let i = 1; i <= data.length; i++) {
+		result[i - 1] = data[data.length - i];
 	}
+	console.log(result);
+    return result;
 }
-// for (let i = 2; i <= 16; i++) {
-//     if (i % 2 === 0) {
-//         continue;
-//     } else {
-//         console.log(i);
-//     }
-// }
-
-function fourthTask() {
-	// Пишем решение вот тут
-    let i = 2;
-	while (i <= 16) {
-		if (i % 2 === 0) {
-			i++;
-			continue;
-		} else {
-			console.log(i);
-		}
-		i++;
-    }
-}
-// 5
-function fifthTask() {
-	const arrayOfNumbers = [];
-
-	for (let i = 0; i <= 5; i++) {
-        arrayOfNumbers[i] = 5 + i;
-    }
-    console.log(arrayOfNumbers);
-	return arrayOfNumbers
-}
-
+thirdTask();
