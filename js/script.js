e0deae18d56a387eba5868276885ba5f72f0c5a8
Methuselah <dstengac@gmail.com>
2024-02-24 14:20:04 +0100

    ex 7

--- js/script.js
+++ js/script.js
@@ -15,6 +15,18 @@ function calculateVolumeAndArea(edge) {
   return result;
 }
 
+// оптимизированная версия
+function calculateVolumeAndArea(edge) {
+  if (typeof edge !== "number" || edge <= 0 || edge !== Math.round(edge)) {
+    return "При вычислении произошла ошибка";
+  }
+
+  const square = Math.pow(edge, 3);
+  const volume = 6 * Math.pow(edge, 2);
+
+  return `Объем куба: ${square}, площадь всей поверхности: ${volume}`;
+}
+
 // Место для второй задачи
 function getCoupeNumber(number) {
   let result;
@@ -33,4 +45,22 @@ function getCoupeNumber(number) {
   result = Math.ceil(number / 4);
 
   return result;
+}
+
+console.log(getCoupeNumber("hello"));
+
+// оптимизированная версия
+function getCoupeNumber(number) {
+  if (
+    typeof number !== "number" ||
+    !Number.isInteger(number) ||
+    number <= 0 ||
+    number >= 36
+  ) {
+    return number <= 0 || number >= 36
+      ? "Таких мест в вагоне не существует"
+      : "Ошибка. Проверьте правильность введенного номера места";
+  }
+
+  return Math.ceil(number / 4);
 }
