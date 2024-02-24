81e0b447caa9b05834dc275af8b8f297203ed95a
Methuselah <dstengac@gmail.com>
2024-02-24 15:38:20 +0100

    ex 8

--- js/script.js
+++ js/script.js
@@ -1,66 +1,22 @@
 "use strict";
 
 // Место для первой задачи
-function calculateVolumeAndArea(edge) {
-  let square, volume, result;
+function getTimeFromMinutes(param) {
+  if (typeof param !== "number" || param < 0 || !Number.isInteger(param)) {
+    return "Ошибка, проверьте данные";
+  }
 
-  if (typeof edge === "number" && edge > 0 && edge === Math.round(edge)) {
-    square = Math.pow(edge, 3);
-    volume = 6 * Math.pow(edge, 2);
-    result = `Объем куба: ${square}, площадь всей поверхности: ${volume}`;
+  if (param <= 600) {
+    return `Это ${Math.trunc(param / 60)} часа и ${param % 60} минут`;
   } else {
-    result = "При вычислении произошла ошибка";
-    return result;
+    return "слишком большое число";
   }
-  return result;
 }
-
-// оптимизированная версия
-function calculateVolumeAndArea(edge) {
-  if (typeof edge !== "number" || edge <= 0 || edge !== Math.round(edge)) {
-    return "При вычислении произошла ошибка";
-  }
-
-  const square = Math.pow(edge, 3);
-  const volume = 6 * Math.pow(edge, 2);
-
-  return `Объем куба: ${square}, площадь всей поверхности: ${volume}`;
-}
+console.log(getTimeFromMinutes(150));
+console.log(getTimeFromMinutes(50));
+console.log(getTimeFromMinutes(0));
+console.log(getTimeFromMinutes(-150));
+console.log(getTimeFromMinutes("helo"));
 
 // Место для второй задачи
-function getCoupeNumber(number) {
-  let result;
-  if (
-    typeof number !== "number" ||
-    number != Math.round(number) ||
-    number < 0
-  ) {
-    result = "Ошибка. Проверьте правильность введенного номера места";
-    return result;
-  }
-  if (number >= 36 || number == 0) {
-    result = "Таких мест в вагоне не существует";
-    return result;
-  }
-  result = Math.ceil(number / 4);
-
-  return result;
-}
-
-console.log(getCoupeNumber("hello"));
-
-// оптимизированная версия
-function getCoupeNumber(number) {
-  if (
-    typeof number !== "number" ||
-    !Number.isInteger(number) ||
-    number <= 0 ||
-    number >= 36
-  ) {
-    return number <= 0 || number >= 36
-      ? "Таких мест в вагоне не существует"
-      : "Ошибка. Проверьте правильность введенного номера места";
-  }
-
-  return Math.ceil(number / 4);
-}
+function findMaxNumber() {}
