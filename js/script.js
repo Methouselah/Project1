3941f793adec299be0985c18b94c0b292a40e8e0
Methuselah <dstengac@gmail.com>
2024-02-27 12:09:06 +0100

    for each

--- js/script.js
+++ js/script.js
@@ -1,24 +1,7 @@
 "use strict";
 
-let person = {
-  name: "Vasia",
-  lastname: "grigoriev",
-  params: {
-    width: 33,
-    old: 44,
-  },
-};
+const arr = [1, 2, 3, 4, 5];
 
-const { width, old } = person.params;
-
-console.log(width);
-
-for (let key in person) {
-  if (typeof person[key] === "object") {
-    for (let i in person[key]) {
-      console.log(person[key][i]);
-    }
-  } else {
-    console.log(person[key]);
-  }
-}
+arr.forEach(function (value, index, arr) {
+  console.log(`${index}: ${value} in ${arr}`);
+});
