7efe6179a88cd46a1609cae7a3ddc417b7174fe8
Methuselah <dstengac@gmail.com>
2024-02-27 12:09:37 +0100

    forEach error

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 "use strict";
 
+arr.forEach(function (value, index, arr) {
+  console.log(`${index}: ${value} in ${arr}`);
+});
+
 const arr = [1, 2, 3, 4, 5];
 
 arr.forEach(function (value, index, arr) {
