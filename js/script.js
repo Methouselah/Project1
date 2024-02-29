947655d5af62bfefad63cc9ec194451a8de2c458
Methuselah <dstengac@gmail.com>
2024-02-29 12:04:04 +0100

    arr

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
 "use strict";
 
-arr = [4, 5, 6, 6, 7, 7, 7];
-
-function clearArr(arr) {
-  for (let i = 0; i <= arr.length; i++) {
-    console.log(arr.pop());
-  }
-}
-
-clearArr(arr);
-console.log(arr);
+arr = [1, "text", {}, [], 4, "text2"];
