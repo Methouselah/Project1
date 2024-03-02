ab3ad647804831b5595f42a5559c0dbda567fdec
Methuselah <dstengac@gmail.com>
2024-03-02 22:43:33 +0100

    Spread

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
 "use strict";
 
-const arr = [1, "text", {}, [], 4, "text2"];
+const arr = [
+  1,
+  "text",
+  {
+    a: 4,
+    b: 5,
+  },
+  [],
+  4,
+  "text2",
+];
+const nums = [2, 4, 5];
 
-let obj = {
-  name: "fin",
-  age: 20,
-};
+const copy = [...arr];
 
-arr.push(obj);
+copy[4] = 11;
+copy[2].b = 11;
 
-console.log(arr[6]);
+console.log(arr);
+console.log(copy);
