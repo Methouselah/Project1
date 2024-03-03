12f0b6057a0d8600f61f80a70febafa5f1831ea9
Methuselah <dstengac@gmail.com>
2024-03-03 17:11:06 +0100

    ex 11

--- js/script.js
+++ js/script.js
@@ -1,35 +1,17 @@
 "use strict";
+const family = ["Peter", "Ann", "Alex", "Linda"];
 
-const personalPlanPeter = {
-  name: "Peter",
-  age: "29",
-  skills: {
-    languages: ["ru", "eng"],
-    programmingLangs: {
-      js: "20%",
-      php: "10%",
-    },
-    exp: "1 month",
-  },
-  showAgeAndLangs: function (plan) {
-    const { age } = plan,
-      { languages } = plan.skills;
-    return `Мне ${age} и я владею языками: ${languages
-      .join(" ")
-      .toUpperCase()}`;
-  },
-};
-
-function showExperience(plan) {
-  return `${plan.skills.exp}`;
-}
-
-function showProgrammingLangs(plan) {
-  const { programmingLangs } = plan.skills;
-  let result = "";
-  for (let key in programmingLangs) {
-    result += `Язык ${key} изучен на ${programmingLangs[key]}\n`;
+function showFamily(arr) {
+  if (arr.length === 0) {
+    return "Семья пуста";
   }
-  return result;
+  return `Семья состоит из: ${arr.join(" ")}`;
 }
-console.log(showProgrammingLangs(personalPlanPeter));
+
+const favoriteCities = ["liSBon", "ROME", "miLan", "Dublin"];
+
+function standardizeStrings(arr) {
+  arr.forEach((element) => {
+    console.log(element.toLowerCase());
+  });
+}
