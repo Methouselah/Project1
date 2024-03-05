bfcee31a21c830084b347da660af6f4da6bd51dd
Methuselah <dstengac@gmail.com>
2024-03-05 21:35:10 +0100

    proto

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+const car = {
+  width: 3.5,
+  color: "white",
+  motor: 350,
+};
+
+const mersedes = {
+  width: 3.0,
+  motor: 250,
+};
+
+mersedes.__proto__ = car; // устаревший способ
+
+Object.setPrototypeOf(mersedes, car);
