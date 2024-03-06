a4995b54dbb96e2e1fa9b239639808a4bb4202fe
Methuselah <dstengac@gmail.com>
2024-03-06 21:04:50 +0100

    leedcode ex3

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 "use strict";
-/**
- * @return {Function}
- */
-var createHelloWorld = function () {
-  return function (...args) {
-    return "Hello World";
+
+var expect = function (val) {
+  return {
+    toBe: function (expected) {
+      if (val !== expected) {
+        throw new Error("Not Equal");
+      }
+      return true;
+    },
+    notToBe: function (notExpected) {
+      if (val === notExpected) {
+        throw new Error("Equal");
+      }
+      return true;
+    },
   };
 };
 
-/**
- * const f = createHelloWorld();
- * f(); // "Hello World"
- */
+console.log(expect(5).toBe(5));
