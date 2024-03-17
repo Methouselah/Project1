88b5896f409169ff9dd3755865ae529b73c801da
Methuselah <dstengac@gmail.com>
2024-03-17 16:52:46 +0100

    maxNumber

--- js/script.js
+++ js/script.js
@@ -3,43 +3,9 @@ const input = document.getElementById("input"),
   output = document.getElementById("output"),
   button = document.getElementById("button");
 
-const students = [
-  "Peter",
-  "Andrew",
-  "Ann",
-  "Mark",
-  "Josh",
-  "Sandra",
-  "Cris",
-  "Bernard",
-  "Takesi",
-];
-
-function sortStudentsByGroups(arr) {
-  const a = [],
-    b = [],
-    c = [],
-    rest = [];
-  arr.sort();
-
-  for (let i = 0; i < arr.length; i++) {
-    if (i < 3) {
-      a.push(arr[i]);
-    } else if (i < 6) {
-      b.push(arr[i]);
-    } else if (i < 9) {
-      c.push(arr[i]);
-    } else {
-      rest.push(arr[i]);
-    }
-  }
-
-  return [
-    a,
-    b,
-    c,
-    `Оставшиеся студенты: ${rest.length !== 0 ? rest.join(", ") : "-"} `,
-  ];
+const arr = [3, 8, 1, 10, 5, 14, 6, 8, 99];
+function maxNumber(num) {
+  return Math.max(...arr);
 }
 
-console.log(sortStudentsByGroups(students));
+output.innerHTML = maxNumber(arr);
