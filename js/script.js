c9c47f2d67a10f16f621add2f3873ca7209b4bbd
Methuselah <dstengac@gmail.com>
2024-03-19 17:59:39 +0100

    maxNamder2

--- js/script.js
+++ js/script.js
@@ -5,7 +5,13 @@ const input = document.getElementById("input"),
 
 const arr = [3, 8, 1, 10, 5, 14, 6, 8, 99];
 function maxNumber(num) {
-  return Math.max(...arr);
+  let max = num[0];
+  for (let i = 1; i < num.length; i++) {
+    if (num[i] > max) {
+      max = num[i];
+    }
+  }
+  return max;
 }
 
 output.innerHTML = maxNumber(arr);
