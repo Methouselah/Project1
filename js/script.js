cd9c5ad71edc6457a4378ec2b3d5dd0ed0609f97
Methuselah <dstengac@gmail.com>
2024-03-25 12:11:54 +0100

    type coercion

--- js/script.js
+++ js/script.js
@@ -3,15 +3,15 @@ const input = document.getElementById("input"),
   output = document.getElementById("output"),
   button = document.getElementById("button");
 
-const arr = [3, 8, 1, 10, 5, 14, 6, 8, 99];
-function maxNumber(num) {
-  let max = num[0];
-  for (let i = 1; i < num.length; i++) {
-    if (num[i] > max) {
-      max = num[i];
-    }
-  }
-  return max;
-}
-
-output.innerHTML = maxNumber(arr);
+// let x = 5; alert( x++ );
+// [ ] + false - null + true;
+// let y = 1; let x = y = 2; alert(x);
+// [ ] + 1 + 2
+// alert( "1"[0] );
+// 2 && 1 && null && 0 && undefined ;
+// !!( a && b ) и (a && b);
+// alert( null || 2 && 3 || 4 );
+// a = [1, 2, 3]; b = [1, 2, 3]; Правда ли что a == b ;
+//  alert( +"Infinity" );
+//  "Ёжик" > "яблоко";
+//  0 || "" || 2 || undefined || true || falsе ;
