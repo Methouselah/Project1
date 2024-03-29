abe7aaf527dc458f4dedcb6f7f4c7ed2ffccd1f4
Methuselah <dstengac@gmail.com>
2024-03-29 11:55:27 +0100

    document

--- js/script.js
+++ js/script.js
@@ -1,17 +1,13 @@
 "use strict";
-const input = document.getElementById("input"),
-  output = document.getElementById("output"),
-  button = document.getElementById("button");
 
-// let x = 5; alert( x++ );
-// [ ] + false - null + true;
-// let y = 1; let x = y = 2; alert(x);
-// [ ] + 1 + 2
-// alert( "1"[0] );
-// 2 && 1 && null && 0 && undefined ;
-// !!( a && b ) и (a && b);
-// alert( null || 2 && 3 || 4 );
-// a = [1, 2, 3]; b = [1, 2, 3]; Правда ли что a == b ;
-//  alert( +"Infinity" );
-//  "Ёжик" > "яблоко";
-//  0 || "" || 2 || undefined || true || falsе ;
+const div = document.querySelector("div");
+const divs = document.querySelectorAll("div");
+const inputs = document.getElementsByTagName("input");
+const input = document.getElementById("input");
+const inputClass = document.getElementsByClassName("input");
+
+console.log(div);
+console.log(divs);
+console.log(inputs);
+console.log(input);
+console.log(inputClass);
