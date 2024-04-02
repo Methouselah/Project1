9a4f5bd58def50c41380323398f4fd4f77acec8a
Methuselah <dstengac@gmail.com>
2024-04-02 18:17:47 +0200

    get element

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 "use strict";
-
+// поиск по селектору (элемент)
 const div = document.querySelector("div");
+
+// поиск по селектору всех элементов (NodeList)
 const divs = document.querySelectorAll("div");
+
+// поиск по названию тега (HTMLCollection)
 const inputs = document.getElementsByTagName("input");
+
+// поиск по id (элемент)
 const input = document.getElementById("input");
+
+// поиск по class (HTMLCollection)
 const inputClass = document.getElementsByClassName("input");
 
-console.log(div);
-
-input.style.borderRadius = "10px";
-input.style.background = "#000";
+const p = document.createElement("p");
+console.log(p);
