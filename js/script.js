7338d2b80e608c85973a8379e2016e3d7a488fb5
Methuselah <dstengac@gmail.com>
2024-04-03 22:27:56 +0200

    ex 44

--- js/script.js
+++ js/script.js
@@ -1,10 +1,51 @@
 "use strict";
 
-const wrapper = document.querySelector(".wrapper");
+/* Задания на урок:
 
-const one = wrapper.querySelectorAll(".one");
-const two = wrapper.querySelectorAll(".two");
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
 
-console.log(wrapper);
-console.log(one);
-console.log(two);
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
+const movieDB = {
+  movies: [
+    "Логан",
+    "Лига справедливости",
+    "Ла-ла лэнд",
+    "Одержимость",
+    "Скотт Пилигрим против...",
+  ],
+};
+
+// __________________ 1 __________________
+
+document.querySelectorAll(".promo__adv img").forEach((item) => {
+  item.remove();
+});
+
+// __________________ 2 __________________
+document.querySelector(".promo__genre").innerHTML = "драма";
+
+// __________________ 3 __________________
+const bg = document.querySelector(".promo__bg");
+
+bg.style.background = `url("../foto/bg.jpg") center / cover no-repeat`;
+
+// __________________ 4 __________________
+const interactiveList = document.querySelector(".promo__interactive-list");
+interactiveList.innerHTML = "";
+movieDB.movies.sort();
+
+movieDB.movies.forEach((item, index) => {
+  interactiveList.innerHTML += `<li class="promo__interactive-item">
+    ${index + 1}. ${item}
+    <div class="delete"></div>
+  </li>`;
+});
