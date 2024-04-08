efe3a78c4e77e4f47e486e0873d1de85da9e5ec5
Methuselah <dstengac@gmail.com>
2024-04-08 20:15:42 +0200

    nav in dom

--- js/script.js
+++ js/script.js
@@ -1,51 +1,14 @@
 "use strict";
 
-/* Задания на урок:
+const dataCurrent = document.querySelector('[data-current="3"]');
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
+const current = document.querySelector("#current");
+// console.log(current.parentNode); // родитель
+// console.log(current.parentNode.parentNode); // родитель родителя
+// console.log(current.previousSibling);
+// console.log(current.nextSibling);
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
-const movieDB = {
-  movies: [
-    "Логан",
-    "Лига справедливости",
-    "Ла-ла лэнд",
-    "Одержимость",
-    "Скотт Пилигрим против...",
-  ],
-};
-
-// __________________ 1 __________________
-
-document.querySelectorAll(".promo__adv img").forEach((item) => {
-  item.remove();
-});
-
-// __________________ 2 __________________
-document.querySelector(".promo__genre").innerHTML = "драма";
-
-// __________________ 3 __________________
-const bg = document.querySelector(".promo__bg");
-
-bg.style.background = `url("../foto/bg.jpg") center / cover no-repeat`;
-
-// __________________ 4 __________________
-const interactiveList = document.querySelector(".promo__interactive-list");
-interactiveList.innerHTML = "";
-movieDB.movies.sort();
-
-movieDB.movies.forEach((item, index) => {
-  interactiveList.innerHTML += `<li class="promo__interactive-item">
-    ${index + 1}. ${item}
-    <div class="delete"></div>
-  </li>`;
-});
+console.log(dataCurrent.previousElementSibling); // предыдущий элемент
+console.log(dataCurrent.nextElementSibling); // следующий элемент
+console.log(document.body.firstElementChild);
+console.log(document.body.lastElementChild);
