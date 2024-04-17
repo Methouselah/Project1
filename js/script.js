5d506e35ce5804f68d2e284f5f3267182f410d0f
Methuselah <dstengac@gmail.com>
2024-04-17 20:43:34 +0200

    setTimeout

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 "use strict";
 
-document.addEventListener("click", () => {
-  const tabs = document.querySelectorAll(".tabheader__item"),
-    tabsContent = document.querySelectorAll(".tabcontent"),
-    tabsParent = document.querySelector(".tabheader__items");
+document.addEventListener("DOMContentLoaded", () => {
+  const output = document.querySelector("#output"),
+    btn = document.querySelector(".button"),
+    input = document.querySelector("input");
+
+  let i = 0;
+  function log() {
+    console.log("lekdfdlmdfld");
+    i++;
+    if (i === 10) {
+      clearTimeout(timer2);
+    }
+    timer2 = setTimeout(log, 100);
+  }
 });
