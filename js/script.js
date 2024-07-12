5503c17c9807eab0214b721acc40782f2c232ed8
Methuselah <dstengac@gmail.com>
2024-07-12 16:34:58 +0200

    test

--- js/script.js
+++ js/script.js
@@ -4,12 +4,4 @@ const output = document.querySelector("#output"),
   btn = document.querySelector(".button"),
   input = document.querySelector("#input");
 
-let leakedArray = [];
-
-function createMemoryLeak() {
-  for (let i = 0; i < 10000; i++) {
-    leakedArray.push(new Array(1000000).join("x"));
-  }
-}
-
-createMemoryLeak();
+console.log(btn);
