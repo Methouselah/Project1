1b09e611af6faae1733e41b3be8cbf2921a7e9a0
Methuselah <dstengac@gmail.com>
2024-07-21 23:39:51 +0200

    started new function hover element

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+document.addEventListener('DOMContentLoaded', function() {
+  const hoverElement = document.querySelector('.header__nav__link');
+
+  hoverElement.addEventListener('mouseover', function() {
+      hoverElement.style.backgroundColor = '#d1e7dd';
+      hoverElement.textContent = 'Mouse is over!';
+  });
+
+  hoverElement.addEventListener('mouseout', function() {
+      hoverElement.style.backgroundColor = '#e9ecef';
+      hoverElement.textContent = 'Hover over me!';
+  });
+});
