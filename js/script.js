707ba0a256e5abac38fd554e81ba40b728a89afa
Methuselah <dstengac@gmail.com>
2024-07-25 21:19:26 +0200

    setup for new project

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const burgerMenu = document.querySelector(".burger-menu"),
-    content = document.querySelector(".content"),
-    body = document.querySelector("body");
-
-  burgerMenu.addEventListener("click", (e) => {
-    if (content.classList[1] == "active") {
-      content.classList.remove("active");
-      burgerMenu.classList.remove("active");
-      body.classList.remove("active");
-    } else {
-      content.classList.add("active");
-      burgerMenu.classList.add("active");
-      body.classList.add("active");
-    }
-  });
-});
+document.addEventListener("DOMContentLoaded", () => {});
